4e13165806182bd1a9d85ca24c3deecf6bd62053
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2025-12-10 19:27:23 -0300

    Commit inicial: inicializando projeto

---
+++ src/test/java/com/testedti/dronedelivery/DronedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testedti.dronedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
